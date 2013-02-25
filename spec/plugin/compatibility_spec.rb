ff0168accb871356ebc0d3efcdea1eb2979faf82
Andrew Radev <andrey.radev@gmail.com>
2013-02-25 20:59:03 +0100

    Move some specs around

--- spec/plugin/compatibility_spec.rb
+++ spec/plugin/compatibility_spec.rb
@@ -18,4 +18,31 @@ describe "compatibility with normal pasting" do
       one
     EOF
   end
+
+  it "can use a register other than the default one" do
+    set_file_contents <<-EOF
+      one
+      two
+    EOF
+
+    vim.search('one').normal('V"ay')
+    vim.search('two').normal('V"by')
+
+    vim.command("call feedkeys('\"ap')")
+    vim.write
+    assert_file_contents <<-EOF
+      one
+      two
+      one
+    EOF
+
+    vim.command("call feedkeys('\"bp')")
+    vim.write
+    assert_file_contents <<-EOF
+      one
+      two
+      one
+      two
+    EOF
+  end
 end
