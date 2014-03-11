45082d4ea11e5ad0d3e24228b8e5e4d553c227aa
Andrew Radev <andrey.radev@gmail.com>
2014-03-11 20:39:34 +0100

    Try starting gVim explicitly
    This may help with the hanging build, though it seems unlikely.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ Vimrunner::RSpec.configure do |config|
   plugin_path = File.expand_path('.')
 
   config.start_vim do
-    vim = Vimrunner.start
+    vim = Vimrunner.start_gvim
 
     vim.add_plugin(plugin_path, 'plugin/whitespaste.vim')
     Support::Vim.define_vim_methods(vim)
